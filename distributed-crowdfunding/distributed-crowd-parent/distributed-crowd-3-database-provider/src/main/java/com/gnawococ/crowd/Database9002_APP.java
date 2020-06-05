f3e804fb2157a23874dee0689a15ef75497ce316
15398069231 <51853925+15398069231@users.noreply.github.com>
2020-06-05 16:52:23 +0800

    [update application.yml and pom.xml]

--- distributed-crowdfunding/distributed-crowd-parent/distributed-crowd-3-database-provider/src/main/java/com/gnawococ/crowd/Database9002_APP.java
+++ distributed-crowdfunding/distributed-crowd-parent/distributed-crowd-3-database-provider/src/main/java/com/gnawococ/crowd/Database9002_APP.java
@@ -14,8 +14,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.gnawococ.crowd.mapper")
-public class Database8002_APP {
+public class Database9002_APP {
     public static void main(String[] args) {
-        SpringApplication.run(Database8002_APP.class,args);
+        SpringApplication.run(Database9002_APP.class,args);
     }
 }
