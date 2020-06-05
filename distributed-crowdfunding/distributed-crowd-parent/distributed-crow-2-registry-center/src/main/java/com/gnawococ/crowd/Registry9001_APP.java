bd8e190d67ffe75282210d5f872a3686cf4b4437
15398069231 <51853925+15398069231@users.noreply.github.com>
2020-06-05 16:53:48 +0800

    [update application.yml and pom.xml]

--- distributed-crowdfunding/distributed-crowd-parent/distributed-crow-2-registry-center/src/main/java/com/gnawococ/crowd/Registry9001_APP.java
+++ distributed-crowdfunding/distributed-crowd-parent/distributed-crow-2-registry-center/src/main/java/com/gnawococ/crowd/Registry9001_APP.java
@@ -13,8 +13,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  **/
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
-public class Registry8001_APP {
+public class Registry9001_APP {
     public static void main(String[] args) {
-        SpringApplication.run(Registry8001_APP.class,args);
+        SpringApplication.run(Registry9001_APP.class,args);
     }
 }
